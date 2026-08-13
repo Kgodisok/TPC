101ddc4df5bceea8f95d51017eb8d3488dcbe956
Matsepe Kgodiso <kgodisomatsepe474@gmail.com>
2026-08-13 08:55:02 +0200

    remove comments from the js file code

--- backend/landingPage.js
+++ backend/landingPage.js
@@ -1,25 +1,3 @@
-/*            id="username" 
-            name="email"
-            placeholder="Enter your email"
-        >
-
-        <br><br>
-
-        <label for="password">Password:</label>
-        <input 
-            type="password" 
-            id="password" 
-            name="password"
-            placeholder="Enter your password"
-        >
-
-    <h1>Learner Supp
-        <button id="welcomeBtn">
-
-        <p id="message"></p>
-
-
-*/
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
